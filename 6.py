5ce2638e07701a902177728523bdef70606b70c7
Belov-Daniil <133764038+Belov-Daniil@users.noreply.github.com>
2023-11-24 17:25:54 +0400

    6.py

--- 6.py
+++ 6.py
@@ -1,24 +1,62 @@
-#У няни 7 разных фруктов (ф1,…ф7). Сформировать (вывести) все возможные варианты меню полдника (1 фрукт) для ребенка на неделю. 
+#. У няни 7 разных фруктов (ф1,…ф7).
+# Сформировать (вывести) все возможные варианты меню полдника
+# (1 фрукт) для ребенка на неделю.
 from itertools import *
+import numpy as np
+
+fruit_basket = ['apple', 'pineapple', 'orange', 'banana', 'pear', 'kiwi', 'avocado']
+
+all_variants = []
 
 
-fruits = ['Груша - 300 калорий', 'Банан - 400 каллорий', 'Яблоко - 150 калорий', 'Персик - 465 калорий', 
-          'Манго - 450 калорий', 'Гранат - 250 калорий', 'Апельсин - 333 калорий']
+def F_iter():
+    for i in permutations(fruit_basket, 7):
+        all_variants.append(i)
+    return all_variants
 
 
-days = ['Понедельник', 'Вторник', 'Среда',
-        'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+def F_rec(a, k):
+    if k == 1:
+        return [[x] for x in a]
+    else:
+        return [[x] + y for x in a for y in F_rec(a, k - 1)]
 
-fruits_list = list(permutations(fruits, fruits.__len__()))
 
-s = ''
+def start():
+    print('У няни 7 разных фруктов (ф1,…ф7). Сформировать (вывести) все возможные варианты меню полдника (1 фрукт) для ребенка на неделю.')
+    a = input('Напишите rec для рекурсивного решения, iter для итеративного \n')
+    while a != 'rec' and a != 'iter':
+        a = input('Напишите rec для рекурсивного решения, iter для итеративного \n')
+    if a == 'rec':
+        p = 0
+        fr = F_rec(fruit_basket, 7)
+        for j in fr:
+            if len(np.unique(j)) < 7:
+                pass
+            else:
+                p += 1
+                print(*j)
+        print('общее кол-во вариантов меню на неделю : ' + str(p))
+        b = ''
+        while b != 'rerun' and b != 'exit':
+            b = input('чтобы начать заного напишите rerun, чтобы закрыть программу напишите exit \n')
+        if b == 'rerun':
+            start()
+        else:
+            exit()
+    else:
+        p = 0
+        fi = F_iter()
+        for j in fi:
+                p += 1
+                print(*j)
+        print('общее кол-во вариантов меню на неделю : ' + str(p))
+        b = ''
+        while b != 'rerun' and b != 'exit':
+            b = input('чтобы начать заного напишите rerun, чтобы закрыть программу напишите exit \n')
+        if b == 'rerun':
+            start()
+        else:
+            exit()
 
-count =0
-for j in range(0, fruits_list.__len__()):
-    for i in range(0, 7):
-        s = days[i] + ": " + str(fruits_list[j][i])
-        print(s)
-        count += 1
-    print('\n')
-print('Возможных расстановок 7 фруктов по дням недели - ' + str(count))
-    
+start()
